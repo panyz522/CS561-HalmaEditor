1f32cf2c87cba261442e8d82de802ccbe6c2556a
Yongzhen Pan <panyz522@gmail.com>
2019-10-13 20:25:07 -0700

    Add camp highlight

--- Data/BoardManager.cs
+++ Data/BoardManager.cs
@@ -9,6 +9,9 @@ namespace HalmaEditor.Data
 {
     public class BoardManager : IDisposable
     {
+        public HashSet<(int, int)> LeftUps = new HashSet<(int, int)>() { (0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (1, 1), (1, 2), (1, 3), (1, 4), (2, 0), (2, 1), (2, 2), (2, 3), (3, 0), (3, 1), (3, 2), (4, 0), (4, 1) };
+        public HashSet<(int, int)> RightDowns = new HashSet<(int, int)>() { (11, 15), (11, 14), (12, 15), (12, 14), (12, 13), (13, 15), (13, 14), (13, 13), (13, 12), (14, 15), (14, 14), (14, 13), (14, 12), (14, 11), (15, 15), (15, 14), (15, 13), (15, 12), (15, 11) };
+
         public event EventHandler RunnerTrigger;
 
         public BoardOptions Options { get; set; }
