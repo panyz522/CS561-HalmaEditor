97375c04a7a7b04c2dd1a106933cf68b4c7036fb
Yongzhen Pan <panyz522@gmail.com>
2019-10-22 19:44:37 -0700

    Fix Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using HalmaEditor.Tools;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace HalmaEditor
 {
@@ -20,9 +29,9 @@ namespace HalmaEditor
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-#if !DEBUG
+                    #if !DEBUG
                     webBuilder.UseContentRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-#endif
+                    #endif
                     webBuilder.UseStartup<Startup>();
                 });
     }
